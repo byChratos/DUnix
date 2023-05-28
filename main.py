d95f1423050bdf3558f80bd8630268246cf8e632
byChratos <byChratos@gmx.de>
2023-05-28 19:58:17 +0200

    Created Button and resizing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class App(tk.Tk):
         self.height = 600
         self.width = 900
         self.geometry('900x600')
-        self.minsize(300, 200)
+        self.minsize(600, 350)
 
         self.title('Discord Unix Timestamp Creator')
         self.font = ("Rubik", 15, "normal")
@@ -17,11 +17,28 @@ class App(tk.Tk):
         self.bind("<Configure>", self.resize)
 
 
-        self.date_frame = tk.Frame(master=self, bg='#333333', width=(900-40)/2)
+        #Frame for Date input
+        self.date_frame = tk.Frame(master=self, bg='#333333', width=(900-40)/2, height=280)
         self.date_frame.grid(row=0, column=0, padx=10, pady=10)
 
-        self.time_frame = tk.Frame(master=self, bg='#333333', width=(900-40)/2)
+        #Frame for Time input
+        self.time_frame = tk.Frame(master=self, bg='#333333', width=(900-40)/2, height=280)
         self.time_frame.grid(row=0, column=1, padx=10, pady=10)
+
+        #Frame for the argument
+        self.arg_frame = tk.Frame(master=self, bg='#333333', width=(900-20), height=180)
+        self.arg_frame.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+
+        #Frame for the "CREATE" Button
+        self.button_frame = tk.Frame(master=self, bg='#333333', width=(900-20), height=80)
+        self.button_frame.pack_propagate(0)
+        self.button_frame.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+        self.create_button = tk.Button(master=self.button_frame, text='Create', command=self.create)
+        self.create_button.pack(fill='both', expand=1)
+
+    def create(self):
+        print("test")
 
     #Widget resizing if window resizes
     def resize(self, event):
@@ -29,8 +46,10 @@ class App(tk.Tk):
             self.height = event.height
             self.width = event.width
 
-            self.date_frame.config(width=round((event.width-40)/2))
-            self.time_frame.config(width=round((event.width-40)/2))
+            self.date_frame.config(width=round((event.width-40)/2), height=round((event.height/100)*52-20))
+            self.time_frame.config(width=round((event.width-40)/2), height=round((event.height/100)*52-20))
+            self.arg_frame.config(width=event.width-20, height=round((event.height/100)*33-20))
+            self.button_frame.config(width=event.width-20, height=round((event.height/100)*15-20))
 
 
 #Main function
