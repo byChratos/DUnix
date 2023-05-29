dc09a7d89663a9db2b460bae4283cbb1ea733399
byChratos <byChratos@gmx.de>
2023-05-29 20:53:08 +0200

    Added Type selection of timestamp

--- main.py
+++ main.py
@@ -8,9 +8,23 @@ class App(tk.Tk):
         #Colors
         self.dark_grey = '#2b2d31'
         self.light_grey = '#313338'
+        self.lighter_grey = '#3a3c40'
+
         self.green = '#2bbf6c'
         self.dark_green = '#23a55a'
         self.light_green = '#31ea7f'
+
+        self.white = '#dbdee1'
+
+        self.blue = '#4752c4'
+        self.light_blue = '#5c6aff'
+        self.lighter_blue = '#635cff'
+
+
+
+        self.debug_blue = '#323ea8'
+        self.debug_red = '#c42142'
+        self.debug_pink = '#e37f93'
 
         #Geometry of window
         self.height = 600
@@ -33,7 +47,7 @@ class App(tk.Tk):
         self.date_frame.grid_propagate(0)
         self.date_frame.grid(row=0, column=0, padx=10, pady=10)
 
-        self.date_label = tk.Label(master=self.date_frame, text='Date', font=("Rubik", round((600/100)*4), "normal"), background=self.light_grey, bd=0, fg='#dbdee1')
+        self.date_label = tk.Label(master=self.date_frame, text='Date', font=("Rubik", round((600/100)*4), "normal"), background=self.light_grey, bd=0, fg=self.white)
         self.date_label.grid(row=0, column=0, padx=8, pady=8)
 
         #Frame for time input
@@ -41,7 +55,7 @@ class App(tk.Tk):
         self.time_frame.grid_propagate(0)
         self.time_frame.grid(row=0, column=1, padx=10, pady=10)
 
-        self.time_label = tk.Label(master=self.time_frame, text='Time', font=("Rubik", round((600/100)*4), "normal"), background=self.light_grey, bd=0, fg='#dbdee1')
+        self.time_label = tk.Label(master=self.time_frame, text='Time', font=("Rubik", round((600/100)*4), "normal"), background=self.light_grey, bd=0, fg=self.white)
         self.time_label.grid(row=0, column=0, padx=8, pady=8)
 
 
@@ -51,8 +65,64 @@ class App(tk.Tk):
         self.arg_frame.grid_propagate(0)
         self.arg_frame.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
-        self.arg_label = tk.Label(master=self.arg_frame, text='Type', font=("Rubik", round((600/100)*4), "normal"), background=self.light_grey, bd=0, fg='#dbdee1')
-        self.arg_label.grid(row=0, column=0, padx=8, pady=8)
+        #Frame for arg label
+        self.arg_label_frame = tk.Frame(master=self.arg_frame, bg=self.light_grey, width=(900-20), height=round(((600/100)*33-20)/3))
+        self.arg_label_frame.grid_propagate(0)
+        self.arg_label_frame.grid(row=0, column=0, padx=0, pady=0)
+
+        #Arg label
+        self.arg_label = tk.Label(master=self.arg_label_frame, text='Type', font=("Rubik", round((600/100)*4), "normal"), background=self.light_grey, bd=0, fg=self.white, justify='left')
+        self.arg_label.grid(row=0, column=0, padx=5, pady=5, sticky='wn')
+
+        #Frame for radio button frames
+        self.radio_frame = tk.Frame(master=self.arg_frame, bg=self.light_grey, width=(900-40), height=round((600/100)*33-60))
+        self.radio_frame.grid_propagate(0)
+        self.radio_frame.grid(row=1, column=0, padx=10, pady=0)
+
+        #Arguments:
+        #<t:1543392060> default                 28 November 2018 09:01
+        #<t:1543392060:t> short time            09:01
+        #<t:1543392060:T> long time             09:01:00
+        #<t:1543392060:d> short date            28/11/2018
+        #<t:1543392060:D> long date             28 November 2018
+        #<t:1543392060:f> short datetime        28 November 2018 09:01
+        #<t:1543392060:F> long datetime         Wednesday, 28 November 2018 09:01
+        #<t:1543392060:R> relative              3 years ago
+        #https://gist.github.com/LeviSnoot/d9147767abeef2f770e9ddcd91eb85aa
+
+
+        #Radio button frames
+        self.radio_button_frames = []
+        c = 0
+        r = 0
+        for i in range(8):
+            self.radio_button_frames.append(tk.Frame(master=self.radio_frame, padx=0, pady=0, width=(900-40)/4, height=round(((600/100)*33-20)/3), bg=self.green))
+            self.radio_button_frames[i].pack_propagate(0)
+            if (i==4):
+                c = 0
+                r = 1
+            self.radio_button_frames[i].grid(row=r, column = c, padx=0, pady=0)
+            c += 1
+
+
+        #Radio buttons
+        test = [('Default', 'default'), ('Short Time', 't'), ('Long Time', 'T'), ('Short Date', 'd'), ('Long Date', 'D'), ('Short Date/Time', 'f'), ('Long Date/Time', 'F'), ('Relative', 'R')]
+        self.buttons = []
+        self.selection = tk.StringVar()
+        self.selection.set('default')
+
+        #Creation of radio buttons
+        i = 0
+        for (name, value) in test:
+            master = self.radio_button_frames[i]
+            self.buttons.append(tk.Radiobutton(master=master, padx=0, pady=0, width=20, font=("Rubik", 12, "normal"), bd=0, text=name, value=value, variable=self.selection, indicatoron=0, bg=self.light_grey, fg=self.white, selectcolor=self.light_blue, activebackground=self.blue, activeforeground=self.white))
+            self.buttons[i].pack(fill='both', expand=1)
+
+            self.buttons[i].bind("<Enter>", self.on_enter_radio)
+            self.buttons[i].bind("<Leave>", self.on_leave_radio)
+
+            i+=1
+
 
 
 
@@ -82,8 +152,17 @@ class App(tk.Tk):
 
             self.date_frame.config(width=round((event.width-40)/2), height=round((event.height/100)*52-20))
             self.time_frame.config(width=round((event.width-40)/2), height=round((event.height/100)*52-20))
+
+
             self.arg_frame.config(width=event.width-20, height=round((event.height/100)*33-20))
+            self.arg_label_frame.config(width=event.width-20, height=round(((event.height/100)*33-20)/3))
+
             self.button_frame.config(width=event.width-20, height=round((event.height/100)*15-20))
+
+            self.radio_frame.config(width=event.width-40, height=round(((event.height/100)*33-20)/3*2))
+
+            for frame in self.radio_button_frames:
+                frame.config(width=round((event.width-40)/4), height=round(((event.height/100)*33-20)/3))
 
     #Button hover color
     def on_enter(self, event):
@@ -93,9 +172,20 @@ class App(tk.Tk):
     def on_leave(self, event):
         self.create_button.config(bg=self.green)
 
+    #Radio button hover color
+    def on_enter_radio(self, event):
+        event.widget['bg'] = self.lighter_grey
+        event.widget['selectcolor'] = self.lighter_blue
+
+    #Radio button change color back to normal
+    def on_leave_radio(self, event):
+        event.widget['bg'] = self.light_grey
+        event.widget['selectcolor'] = self.light_blue
+
+
     #Create button logic
     def create(self):
-        print("test")
+        print(self.selection.get())
 
 
 #Main function
